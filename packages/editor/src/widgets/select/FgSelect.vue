4dfabd40c73ce8794ef0548b8c52293a8dd61a59
yangdongnan <yangdongnan@yangdongnandeMacBook-Air.local>
2026-06-17 11:09:32 +0800

    fix(editor): select 使用 --el-component-size 控制双层高度

--- packages/editor/src/widgets/select/FgSelect.vue
+++ packages/editor/src/widgets/select/FgSelect.vue
@@ -26,6 +26,7 @@ const resolvedOptions = computed(() =>
 const dynamicStyle = computed(() => ({
   width: '100%',
   height: `${widgetData.value.position?.h ?? 32}px`,
+  '--el-component-size': `${widgetData.value.position?.h ?? 32}px`,
   fontSize: widgetStyle.value?.fontSize as string,
   color: widgetStyle.value?.color as string,
 }))
